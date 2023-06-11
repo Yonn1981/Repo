f618db3e8a60a3ab9fca105a16525c0fcd503c55
Yonn1981 <105945713+Yonn1981@users.noreply.github.com>
2023-06-11 08:58:08 +0300

    5.8.4i

--- repo/plugin.video.matrix/resources/hosters/govid.py
+++ repo/plugin.video.matrix/resources/hosters/govid.py
@@ -3,7 +3,7 @@ from resources.lib.parser import cParser
 from resources.lib.comaddon import dialog
 from resources.hosters.hoster import iHoster
 from resources.lib.packer import cPacker
-from resources.lib.comaddon import VSlog
+
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:61.0) Gecko/20100101 Firefox/61.0'
 
@@ -14,11 +14,9 @@ class cHoster(iHoster):
 			
     def setUrl(self, sUrl):
         self._url = str(sUrl)
-        VSlog(sUrl)
         if '/down/'  in sUrl:
             self._url = self._url.replace("/2down/","/play/").replace("/down/","/play/")
     def _getMediaLinkForGuest(self):
-        VSlog(self._url)
         sReferer = ""
         surl = self._url.split('|Referer=')[0]
         sReferer = self._url.split('|Referer=')[1]
@@ -39,20 +37,22 @@ class cHoster(iHoster):
             oRequest.addHeaderEntry('Referer', surl)
             oRequest.addHeaderEntry('User-Agent', UA)
             sHtmlContent2 = oRequest.request()
-            VSlog(sHtmlContent2) 
-            sPattern = '(https.+?m3u8)'
+            sPattern = ',NAME="(.+?)",.+?(https.+?m3u8)'
             aResult = oParser.parse(sHtmlContent2, sPattern)
+            list_url=[]
+            list_q=[]
             for aEntry in aResult[1]:
-            
-                api_call = aEntry
+                list_q.append(aEntry[0]) 
+                list_url.append(aEntry[1]) 
+				
+            api_call = dialog().VSselectqual(list_q,list_url)
 
 
-                if api_call:
-                   return True, api_call+ '|User-Agent=' + UA+'&AUTH=TLS&verifypeer=false' + '&Referer=' + surl
+            if api_call:
+                return True, api_call+ '|User-Agent=' + UA+'&AUTH=TLS&verifypeer=false' + '&Referer=' + surl
 
         sPattern =  'sources: (.+?),' 
         aResult = oParser.parse(sHtmlContent,sPattern)
-        VSlog(sHtmlContent)
         if aResult[0]:
             for aEntry in aResult[1]:
             
